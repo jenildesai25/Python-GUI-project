1feed4211ed72caba756eea8fdfa3bac5da10a29
jenildesai25 <jenildesai25@users.noreply.github.com>
2017-10-09 03:42:52 +0000

    20171009T034210Z

---
+++ mario.c
@@ -0,0 +1,21 @@
+#include<cs50.h>
+#include<stdio.h>
+
+int main(void){
+    int n;
+    do{
+        n = get_int();
+    }while(n > 0 && n >=23);
+    for (int k=0;k<n;k++)
+    {
+        for(int i = 0; i < n-1-k ; i++)
+        {
+            printf(" ");
+        }
+            for(int j = 0 ; j < n-2+k ; j++)
+            {
+                printf("#");
+            }
+    printf("\n");
+    }
+}
